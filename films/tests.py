9155bab9157624c3054d8e36fdda12689808ab26
AnasAbuHamideh <anas.abuhamida@gmail.com>
2022-06-03 17:51:16 +0300

    add tests

--- films/tests.py
+++ films/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
 
-# Create your tests here.
+class TestFilmView(TestCase):
+
+    def setUp(self):
+        self.client.login(username="admin", password="abcd1234")
+    
+    def test_authetication_required(self):
+        self.client.logout()
+        url = reverse('flims_list')
+        # 403 Forbidden
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
